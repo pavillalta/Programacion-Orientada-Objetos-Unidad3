90c5dbbea77adf8257dc8107e6b789f40f2944c1
Pedro Antonio Villalta <pavillalta@ugb.edu.sv>
2022-11-29 20:48:31 -0600

    Agregar archivos de proyecto.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("CodigoEstudiante1-CodigoEstudiante2_Unidad3")>
+<Assembly: AssemblyDescription("Tarea de semana 3 realizada por NombreEstudiante1-NombreEstudiante2")>
+<Assembly: AssemblyCompany("UGB")>
+<Assembly: AssemblyProduct("Poyecto de formularios para el uso de POO")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("NombreEstudiante1-NombreEstudiante2")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("c1bed2a7-92f8-4368-a0fe-3fbbbce04381")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
